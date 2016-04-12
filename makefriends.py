d64ac647c2812cf7db956be0119dfff84681b693
Alexander WONG <awong86@linux.CS.McGill.CA>
2016-04-11 23:14:52 -0400

    starting to work on makefriend.py

--- makefriends.py
+++ makefriends.py
@@ -1,1 +1,26 @@
-m
+#!/user/bin/python
+
+import cgi
+import cgitb;
+cgitb.enable()
+
+from = cgi.FieldStorage()
+
+print """
+	<!DOCTYPE html>
+	<html>
+		<title>Make a Friend!</title>
+		<body bgcolor="ECFAFF"><center>
+			<h2>Make a Friend by choosing from the list below</h2>
+	"""
+f = open('users.txt', 'r')
+for line in f:
+        i=0
+        if i%4:
+                print """<input type="checkbox" name="new_friend" value="""line""">"""line"""<br>"""
+			</form>
+	</center></body>
+	</html>
+	"""
+
+def addFriend():
