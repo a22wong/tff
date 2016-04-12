6248a1e0057c07fa30d364513b61977beff3aa35
Ethan COHN <ecohn@trusty>
2016-04-11 21:21:26 -0400

    added html

--- dashboard.py
+++ dashboard.py
@@ -4,29 +4,48 @@ import cgi, cgitb
 
 cgitb.enable()
 
-print """<html>
+form=cgi.FieldStorage()
+
+name=form.getvalue('fullname')
+username=form.getvalue('username')
+job=form.getvalue('job')
+
+def head():
+
+	print"""
+	Content-Type:text/html\n\n
+	<html>
 
 	<head>
 		<title> tff Dashboard </title> 
 	</head>
 
 	<body bgcolor="ECFAFF">
-	
+	<font face="Arial Rounded MT Bold" color="2340FA" size="3">
+                <p align="left">
+	"""
+	print "
+	%s <br> %s <br>
+	", name, job
+
+	print """
 	<font face="Arial Rounded MT Bold" color="2340FA" size="3">
 		<p align="right">
-		<a href="http://www.cs.mcgill.ca/~ecohn/tff/makefriend.html">
+		<a href="../tff/makefriend.html">
                         Make a Friend
                 </a>
 		&emsp;
-		<a href="http://www.cs.mcgill.ca/~ecohn/tff/seefriend.html">
+		<a href="../tff/seefriend.html">
                         See a Friend
                 </a>
 		&emsp;
-		<a href="http://www.cs.mcgill.ca/~ecohn/tff/index.html">
+		<a href="../tff/index.html">
                         Logout
                 </a>
 		</p>
-	<center><form name="status" action="status.py" method="get">
+	"""
+def form():
+	print """ <center><form name="status" action="status.py" method="get">
 
 		<font face="Arial Rounded MT Bold" color="FF846A" size="4"><b>
 			Update status: 
@@ -41,3 +60,15 @@ print """<html>
 
 	</body>
 </html>"""
+
+def statuses():
+
+
+
+
+#main method
+if __name__="__main__":
+	try:
+		head()
+		form()
+		
