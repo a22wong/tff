50ed716e2988dd14ae8d0724e2592d1a2c0bba00
Annastasia Statakos <annastasia.stathakos@mail.mcgill.ca>
2016-04-12 02:56:12 -0400

    haha lol

--- register.c
+++ register.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 #define FDATA "./users.txt"
 #define MAX 250
-//psuedo structure of registration page
 /* 
-A. validate username -- isUnique()
+	A. validate username -- isUnique()
 	- if no unique: error1 displays links to landing and login pages
 	  else append username and profile info to users.txt file and display success page
 */
@@ -20,6 +20,7 @@ int isUnique(char *username, char *line) {
 	/* username is not unique if d == 0 */
 }
 
+/* retrives a line from a file */
 void myfgets(char *array, int limit, FILE *p) {
 	int i=0, c;
 	if (p == NULL) { array[0]='\0'; return; }
@@ -33,112 +34,111 @@ void myfgets(char *array, int limit, FILE *p) {
 	
 }
 
-int isValidUser(char *uname, FILE *p);
+/* note to anna: dont use this code */
+int unQstring(char *str) {
+	unsigned int i;
+	char tmp[MAX];
+	char *ptr = tmp;
+	memset(tmp, 0, sizeof(tmp));
+
+	for (i=0; i <strlen(str); i++) {
+		if (str[i] != '%') { *ptr++ = str[i]; continue;  }
+		if (!isdigit(str[i+1]) || !isdigit(str[i+2])) { *ptr++ = str[i]; continue; }
+		*ptr++ = ((str[i+1] - '0') << 4) | (str[i+2] - '0');
+		i += 2;
+	}
+	*ptr = '\0';
+	strcpy(str, tmp);
+	return 0;
+}
+
 void printSuccess() {
 	printf("Content-Type: text/html; charset:iso-8859-1\n\n"
 	"<html>"
 	"<head><title> Registration Success </title></head>"
 	"<body>"
-	"\n Registration was a success >> link to dashboard page \n"
+	"<center><h3> Welcome to Tinder for Friends </h3> </center><br>"
+//	"\n Registration was a success >> link to dashboard page \n"
+	"<a href='../tff/login.html'>Click here to log-in</a>"
 	"</body>"
-	"</html>"
-	);
+	"</html>");
 }
 void printFailure() {
 	printf("Content-Type: text/html; charset:iso-8859-1\n\n"
 	"<html><head><title> Registration Failure </title></head>"
 	"<body>"
-	"\nregistration failed >> link to login page or welcome page\n"
+//	"\nregistration failed >> link to login page or welcome page\n"
+	"<center><h3>We're Sorry Jargon</h3></center>"
+	"<p>Your profile could not be made. Choose a better Username"
+	"<a href='../tff/login.html'>Click here to try again</a><br>"
+	"<a href=='../tff/index.html'>Return to Welcome page</a>"
 	"</body></html>");
 }
 
-
-void printTop() {
-	printf("<!DOCTYPE HTML>\n\n"
-	"<html lang='en'>"
-            "<head>"
-                "<meta name='author' content='Annastasia Stathakos'>"
-                "<meta name='description' content='success page for registration'>"
-                "<meta charset='UTF-8'>"
-                "<title> Registration Page </title>"
-
-                "<!-- will make a style sheet that matches whatever ethan designs -->"
-                "<link rel='stylesheet' type='text/css' href='style.css'>"
-            "</head>"
-	"</body>");
-}
-
-void printTail() {
-	printf("</body></html>");
-}
-
 int main(void) {
-        /* pretending to read from html form */
-        char *uName = "Username";
-        char *uname = "username";
+//	char *uName=(char *)malloc(sizeof(char)*MAX);
 	char *data;
-
 	data = getenv("QUERY_STRING");
 	if (data == NULL) {
-		printf("<p>Error! Error in passing data from form to script.\n");
+		/* "ERROR: Unsuccessful data transfer from form to script.\n" */
+		printf("ERROR: QUERY_STRING not found.\n"); return 1;
+//		printf("Content-Type: text/html; charset:iso-8859-1\n\n"
+//		"<html>"
+//		"<head><title> QUERY_STRIG ERROR </title></head>"
+//		"<body><p>ERROR: Unsuccessfull data transfer from form to script.</p></body></html>");
 	} else {
-		/* i wanna see what the query string looks like so:  */
-		printf("%s\n", data);
+	// this doesn't work because there are no spaces in qeury string
+//		sscanf(data,"user=%s",uName);
+		printf("%s",data);
+		unQstring(data);
+		printf("%s",data);
 	}
 
-        FILE *p = fopen(FDATA, "rw");
-        char *line;
-        int r_status;
-        line = (char *)malloc(sizeof(char)*100);
+//        FILE *p = fopen(FDATA, "rw");
+//        char *line;
+//        int r_status;
+//        line = (char *)malloc(sizeof(char)*MAX);
 
-        if (p == NULL) {
-                printf("Congrats you're our first subscriber!\n");
-	/* write data into new users.txt file */
-		FILE *users;
-		users = fopen("users.txt", "w");
-		if (users == NULL) {
-			printf("ERROR: Cannot open users.txt.\n");
-			return EXIT_FAILURE;
-		} 
+//        if (p == NULL) {
+////                printf("Congrats you're our first subscriber!\n");
+//	/* write data into new users.txt file */
+//		FILE *users;
+//		users = fopen("users.txt", "w");
+//		if (users == NULL) {
+////			printf("ERROR: Cannot open users.txt.\n");
+//			return EXIT_FAILURE;
+//		} 
 //		fprintf(fp, "%s", data);
-		fprintf(users,"%s","FULLNAME\nJOB DESCRIPTION\nUSERNAME\nPASSWORD\n");		
-                return 0;
-        }
-
-        myfgets(line,MAX-1,p);
-	myfgets(line,MAX-1,p);
-	myfgets(line,MAX-1,p);
-        while(!feof(p)) {
-                printf("comparing ... %s to %s\n", uName, line);
-                /* if isUnique returns 0, then person has entered a pre-existing username */
-                if (!isUnique(uName, line)) {
-                        printf("%s matches %s! Cannot create user profile.\n", uName, line);
-                        r_status = 0;
-                } else {
-                        printf("%s does not match %s\n", uName, line);
-                        r_status = 1;
-                }
-                int i;
-		myfgets(line,MAX-1,p);
-		myfgets(line,MAX-1,p);
-		myfgets(line,MAX-1,p);
-		myfgets(line,MAX-1,p);
-        }
-        fclose(p);
+////		fprintf(users,"%s","FULLNAME\nJOB DESCRIPTION\nUsername\nPASSWORD\n");		
+//                return 0;
+//       } else {
+////		printf("checking if %s is unique...\n",uName);
+//		int i;
+//		for (i=0;i<3;i++) { myfgets(line,MAX-1,p); }
+//	        while(!feof(p)) {
+// //   	        	printf("comparing ... %s to %s\n", uName, line);
+//  	        	/* if isUnique returns 0, then person has entered a pre-existing username */
+//              		if (!isUnique(uName, line)) {
+//    //            		printf("%s matches %s! Cannot create user profile.\n", uName, line);
+//                		r_status = 0;
+//               	  	} else {
+//      //                  	printf("%s does not match %s\n", uName, line);
+//                        	r_status = 1;
+//                	}
+//                for (i=0;i<4;i++) { myfgets(line,MAX-1,p); }
+//        	}
+//	}
+//        fclose(p);
         printf("compile completed.\n");
 
-        if (r_status) {
-                /* redirect to new user profile */
-            //    printTop();
-              printf("\nThe user profile has been created.\n");
-	      printSuccess();
-          //      printTail();
-        } else {
-                /* redirect to login/registration page */
-              //  printTop();
-              printf("\nThe user profile was not made.\n");
-	      printFailure();
-              //  printTail();
-        }
+//        if (r_status) {
+//                /* redirect to new user profile */
+//              printf("\nThe user profile has been created.\n");
+//	      printSuccess();
+//        } else {
+//                /* redirect to login/registration page */
+//              printf("\nThe user profile was not made.\n");
+//	      printFailure();
+//        }
         return 0;
 }
