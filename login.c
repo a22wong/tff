15b6078dfc02adcb0da6c7b7192f63246e641d64
Annastasia Statakos <annastasia.stathakos@mail.mcgill.ca>
2016-04-12 20:58:18 -0400

    temporary cgbin thing

--- login.c
+++ login.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+#define FDATA "./users.txt"
+#define MAX 250
 //psuedo structure of the login.c file
 /* 
 possible errors:
@@ -18,32 +21,47 @@ B. Successfull Login (returned no errors)
 	i. generates user's dashboard page
 */
 
-int isValidUser(char *usrname);
-int passMatches(char *pass, char *validname);
+//int isValidUser(char *usrname);
+//int passMatches(char *pass, char *validname);
 
 int main(int argc, char *argv[]) {
 
-	FILE *p;
-	char line[2000];
+	printf("Content-Type: text/html\n\n"
+	"<html><head><title> Login </title></head>"
+	"<body>");
+	
+//	int i;	
+//	FILE *p;
+//	char *fline;
+//	fline = (char *)malloc(sizeof(char)*MAX);
+//	char *input;
+//	input = getenv("QUERY_STRING");
+//
+//	p = fopen(FDATA,"r");
+//	if (p == NULL) {
+//		printf("ERROR: Unable to open file\n");
+//		return 2;
+//	}
 
-	if (argc != 2) {
-		printf("Usage: ./a.out filename\n");
-		return 1;
-	} 
+//	for (i=0;i<3;i++) { myfgets(fline,MAX-1,p); }
+//	while (!feof(p)) {
+//		/* parse through the file to check that username exists */
+//		if (  ) {
+//			return 1;
+//		} else {
+//			/* read the next line to check if password matches that users password */
+//			if (/* if inputpassword doesn't match filepassword  */) {
+//				return 2;
+//			} else {
+//				/* login successful: printf html login page. */
+//				printf("login successfull\n");
+//			}
+//		}
+//	}
+//	fclose(p);
 
-	p = fopen(argv[1],"rt");
-	if (p == NULL) {
-		printf("Invalid filename to compile.\n");
-		return 2;
-	}
-
-	myfgets(line,1999,p);
-	while (!feof(p)) {
-		if () {
-
-		}
-	}
-	fclose(p);
+	printf("<a href='./dashboard.py'>text</a> ");
+	printf("</body></html>");
 
 	return 0;
 }
